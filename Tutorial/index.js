800d5db6cde3d15f7056b8e6dbafb728a7ebdb00
Armando Reyes <armando.reyes@welldex.com>
2022-06-17 13:13:23 -0500

    instalando shelljs

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -19,38 +19,56 @@ function initStorage() {
 //este cambio es de mi prueba
 //cambio 2
 //cambio desde master
-export default {
-  getData: async (req, res) => {
-    try {
-      const bigQuery = initBigQuery();
-      const query = `SELECT * FROM pruebanode-352614.prueba_datos.datos LIMIT 1000`;
-      //consulta a BigQuery la informacion de la tabla debe ir entre backticks
-      const options = {
-        query: query,
-        location: 'US',
-      };
-      const [job] = await bigQuery.createQueryJob(options);
-      const [rows] = await job.getQueryResults();
-      res.status(200).json(new Response({ result: true, data: rows }))
-    } catch (error) {
-      console.log(error)
-      res.status(error.code || 500).json(new Response({ result: false, message: 'Error con Google BigQuery', data: error.message }))
-      return
-    }
-  },
-  getBucktData: async (req, res) => {
-    try {
-      // inicializar instancia de Storage
-      const storage = initStorage();
-      // nombre del contenedor al que nos vamos a conectar
-      const bucketName = 'contenedor-prueba';
-      // Metadatos del contenedor para probar que tenemos conexion
-      const [metadata] = await storage.bucket(bucketName).getMetadata();
-      res.json(new Response({ result: true, data: metadata }))
-    } catch (error) {
-      console.log(error)
-      res.status(error.code || 500).json(new Response({ result: false, message: 'Error con Google Storage', data: error.message }))
-      return
-    }
-  }
+// export default {
+//   getData: async (req, res) => {
+//     try {
+//       const bigQuery = initBigQuery();
+//       const query = `SELECT * FROM pruebanode-352614.prueba_datos.datos LIMIT 1000`;
+//       //consulta a BigQuery la informacion de la tabla debe ir entre backticks
+//       const options = {
+//         query: query,
+//         location: 'US',
+//       };
+//       const [job] = await bigQuery.createQueryJob(options);
+//       const [rows] = await job.getQueryResults();
+//       res.status(200).json(new Response({ result: true, data: rows }))
+//     } catch (error) {
+//       console.log(error)
+//       res.status(error.code || 500).json(new Response({ result: false, message: 'Error con Google BigQuery', data: error.message }))
+//       return
+//     }
+//   },
+//   getBucktData: async (req, res) => {
+//     try {
+//       // inicializar instancia de Storage
+//       const storage = initStorage();
+//       // nombre del contenedor al que nos vamos a conectar
+//       const bucketName = 'contenedor-prueba';
+//       // Metadatos del contenedor para probar que tenemos conexion
+//       const [metadata] = await storage.bucket(bucketName).getMetadata();
+//       res.json(new Response({ result: true, data: metadata }))
+//     } catch (error) {
+//       console.log(error)
+//       res.status(error.code || 500).json(new Response({ result: false, message: 'Error con Google Storage', data: error.message }))
+//       return
+//     }
+//   }
+// }
+async function getBucktData() {
+    // inicializar instancia de Storage
+    const storage = initStorage();
+    // nombre del contenedor al que nos vamos a conectar
+    const fileName = '10/fiscal/kitten.png';
+    const destFileName = 'kitten.png';
+    const options = {
+      destination: destFileName,
+    };
+    const bucketName = 'contenedor-prueba';
+    // Metadatos del contenedor para probar que tenemos conexion
+    const contents = await storage.bucket(bucketName).file(fileName).download(options);
+
+  console.log(
+    `Contents of gs://${bucketName}/${fileName} downloaded to ${contents}`
+  );
 }
+getBucktData();
